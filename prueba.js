8ca5871099732cb80958cfbefbc070fedaad7070
OyasumiiAlex <b181010@unach.mx>
2024-08-21 14:12:59 -0600

    chore(add): get min amount function
    version 1.0.2-beta

--- prueba.js
+++ prueba.js
@@ -20,4 +20,27 @@ function calcMotor(tipoNomina, fechaPrimerEmpleo, genero) {
         recomendacionLinea
     };
 }
+function obtenerMontoMinimo(tipoNomina, meses, genero) {
+    const montosMinimos = {
+        m: {
+            A: [100, 400, 900, 100, 600],
+            B: [1000, 600, 1000, 1000, 1000],
+            C: [400, 200, 200, 1000, 600],
+            D: [400, 300, 500, 900, 1000]
+        },
+        f: {
+            A: [800, 800, 800, 600, 200],
+            B: [800, 700, 100, 600, 700],
+            C: [200, 900, 700, 800, 100],
+            D: [500, 1000, 600, 400, 700]
+        }
+    };
 
+    let index = 0;
+    if (meses >= 28) index = 4;
+    else if (meses >= 27) index = 3;
+    else if (meses >= 26) index = 2;
+    else if (meses >= 25) index = 1;
+
+    return montosMinimos[genero][tipoNomina][index];
+}
